cce1411cec77b33dc76f09470c80571d0398976d
cow <zeropl@pm.me>
2023-03-28 21:20:30 -0500

    docstuff

--- terp.lisp
+++ terp.lisp
@@ -23,27 +23,34 @@
     :accessor pen-down)))
 
 (defun init-func (turtle)
+  "intialize functions with the turtle object"
   (defun to-radians (angle)
+    "basic trig shit"
     (* angle (/ pi 180)))
 
   (defun goto (x y &optional (angle 0))
+    "moves to point without drawing a line, I think"
     (setf (x turtle) x)
     (setf (y turtle) y)
     (setf (angle turtle) angle))
 
   (defun toggle-pen ()
+    "toggles pen up and down"
     (if (pen-down turtle)
       (setf (pen-down turtle) nil)
       (setf (pen-down turtle) t)))
 
   (defun left (degrees)
+    "add degrees to turtle angle"
     (let ((a (angle turtle)))
       (setf (angle turtle) (+ a degrees))))
 
   (defun right (degrees)
+    "right is one backwards left"
     (left (- degrees)))
 
   (defun forward (steps)
+    "move forward at angle. if pen-down is t, draw a line"
     (let ((x (x turtle))
           (y (y turtle))
           (angle (angle turtle)))
@@ -60,14 +67,17 @@
         (stroke))))
 
   (defun back (steps)
+    "backin up"
     (forward (- steps))))
 
 (defun init (w h &optional (title "terp.png"))
+  "initialize some globals"
   (defparameter *width* w)
   (defparameter *height* h)
   (defparameter *file* title))
 
 (defmacro terp-go (&rest body)
+  "the basic terrapin macro. Send terp wherever you want, with whatever code."
   `(let ((turtle (make-instance 'terp :x (/ *width* 2) :y (/ *height* 2))))
      (init-func turtle)
      (with-canvas (:width *width* :height *height*)
