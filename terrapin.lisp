53edcd576a7d40a1afc3f7bb0fb5d4fea850bc4c
cow <zeropl@pm.me>
2023-05-21 19:26:16 -0500

    did things more smarter like getting rid of that retarded init-func function

--- terrapin.lisp
+++ terrapin.lisp
@@ -1,5 +1,3 @@
-;;; the terp object ;;;
-
 (in-package #:terrapin)
 
 (defclass terp ()
@@ -21,63 +19,60 @@
     :accessor pen)))
 
 
-(defun init-func (turtle)
-  "intialize functions with the turtle object"
-  (defun to-radians (angle)
-    "basic trig shit"
-    (* angle (/ pi 180)))
+(defun to-radians (angle)
+  "basic trig shit"
+  (* angle (/ pi 180)))
 
-  (defun goto (x y &optional (angle 0))
-    "moves to point without drawing a line, I think"
-    (setf (x turtle) x)
-    (setf (y turtle) y)
-    (setf (angle turtle) angle))
+(defmacro goto (x y &optional (turtle 'turtle))
+  "moves to point without drawing a line"
+  `(setf (x ,turtle) ,x)
+  `(setf (y ,turtle) ,y))
 
-  (defun toggle-pen ()
-    "toggles pen up and down"
-    (if (pen turtle)
-      (setf (pen turtle) nil)
-      (setf (pen turtle) t)))
 
-  (defun pen-up ()
-    (setf (pen turtle) nil))
+(defmacro toggle-pen (&optional (turtle 'turtle))
+  "toggles pen up and down"
+  `(if (pen ,turtle)
+     (setf (pen ,turtle) nil)
+     (setf (pen ,turtle) t)))
 
-  (defun pen-down ()
-    (setf (pen turtle) t))
+(defmacro pen-up (&optional (turtle 'turtle))
+  `(setf (pen ,turtle) nil))
 
-  (defun right (degrees)
-    "add degrees to turtle angle"
-    (setf (angle turtle) (+ (angle turtle) degrees)))
+(defmacro pen-down (&optional (turtle 'turtle))
+  `(setf (pen ,turtle) t))
 
-  (defun left (degrees)
-    "left is one backwards right"
-    (right (- degrees)))
+(defmacro right (degrees &optional (turtle 'turtle))
+  "add degrees to turtle angle"
+  `(setf (angle ,turtle) (+ (angle ,turtle) ,degrees)))
 
-  (defun forward (steps)
-    "move forward at angle. if pen-down is t, draw a line"
-    (let ((x (x turtle))
-          (y (y turtle))
-          (angle (angle turtle)))
-      (let ((dest-x x)
-            (dest-y y))
-        (setf dest-x (+ dest-x (* steps (cos (to-radians angle)))))
-        (setf dest-y (- dest-y (* steps (sin (to-radians angle)))))
-        (if (pen turtle)
-          (progn
-            (v:move-to (x turtle) (y turtle))
-            (v:line-to dest-x dest-y)))
-        (setf (x turtle) dest-x)
-        (setf (y turtle) dest-y)
-        (v:stroke))))
+(defmacro left (degrees &optional (turtle 'turtle))
+  "left is one backwards right"
+  `(right (- ,degrees) ,turtle))
 
-  (defun back (steps)
-    "backin up"
-    (forward (- steps))))
+(defmacro forward (steps &optional (turtle 'turtle))
+  "move forward at angle. if pen-down is t, draw a line"
+  `(let ((x (x ,turtle))
+         (y (y ,turtle))
+         (angle (angle ,turtle)))
+     (let ((dest-x x)
+           (dest-y y))
+       (setf dest-x (+ dest-x (* ,steps (cos (to-radians angle)))))
+       (setf dest-y (- dest-y (* ,steps (sin (to-radians angle)))))
+       (if (pen ,turtle)
+         (progn
+           (v:move-to (x ,turtle) (y ,turtle))
+           (v:line-to dest-x dest-y)))
+       (setf (x ,turtle) dest-x)
+       (setf (y ,turtle) dest-y)
+       (v:stroke))))
+
+(defmacro back (steps &optional (turtle 'turtle))
+  "backin up"
+  `(forward (- ,steps) ,turtle))
 
 (defmacro init-terp (name x y)
   "creates a turtle object"
   `(defvar ,name (make-instance 'terp :x ,x :y ,y)))
-
 
 (defun init (w h &optional (title "terapin.png"))
   "initialize some globals"
@@ -93,7 +88,6 @@
 
 (defmacro terp-go (&rest body)
   "the basic terrapin macro. Send terp wherever you want, with whatever code."
-  (init-func turtle)
   (reset-terp turtle)
   `(v:with-canvas (:width *width* :height *height*)
      (v:rectangle 0 0 *width* *height*)
